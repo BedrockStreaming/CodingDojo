835b8a98c726a5cbebadb18e7fc395c710b13b4c
Valentin CLARAS <valentin-claras@users.noreply.github.com>
2022-04-25 16:57:57 +0200

    fix: update tips description

--- KataVista/utils/Description/Tips.php
+++ KataVista/utils/Description/Tips.php
@@ -10,6 +10,6 @@ class Tips
     public const HELPER_GROUND = 'There is a helper function to check that a landmark is in a specific plan (foreground/middleground/background).';
     public const HELPER_NUMBER = 'There is a helper function to check that a landmark is present multiple times.';
     public const HELPER_STACK = 'There is a helper function to check that a landmark is stacked over another one.';
-    public const HELPER_RELATIVE = 'The new rule can help  position previously discovered elements. This might be the time for some refactoring in the Painter.';
-    public const YOLO = 'At this point you should be able to got a good look at a coding dojo adn TDD. Good luck trying to fit this new rule ;)';
+    public const HELPER_RELATIVE = 'The new rule can help position previously discovered elements. This might be the time for some refactoring in the Painter.';
+    public const YOLO = 'At this point, you should have had a good first look at what a coding dojo is, and how TDD works. Good luck trying to fit this new rule ;)';
 }
