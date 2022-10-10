ba35379ebb15b25f4fe617283d082bd887f7d9be
Orann Weber <orann.weber@gmail.com>
2022-10-10 10:13:34 +0200

    refactor: removed solutions for a test

--- KataCola/utils/InsertCoinCommand.php
+++ KataCola/utils/InsertCoinCommand.php
@@ -39,34 +39,6 @@ class InsertCoinCommand extends Command
 
         $insertedCoins = $input->getArgument(self::COINS);
 
-        $insertedCoins = explode(',',$insertedCoins);
-        $credit = 0;
-        $invalidCoin = 0;
-        foreach ($insertedCoins as $coin) {
-            //1euro
-            if ((int)$coin === 1) {
-                $coin = 100;
-            }
-            //2euroS
-            if ((int)$coin === 2) {
-                $coin = 200;
-            }
-
-            if ((int)$coin > 1 && (int)$coin < 10) {
-                $invalidCoin += (int)$coin;
-                $coin = 0;
-            }
-            $credit  = $credit + (int)$coin;
-        }
-
-        $formattedCoinReturn =  $invalidCoin > 0 ? ' COIN-RETURN '.number_format($invalidCoin / 100, 2): '';
-
-        $creditToSave = (float)($actualCredit + $credit / 100);
-        $yaml = Yaml::dump(['credit' => $creditToSave]);
-        file_put_contents(__DIR__.'/credit.yaml', $yaml);
-
-        $output->writeln('CREDIT: ' . number_format($creditToSave, 2) .$formattedCoinReturn);
-
         return Command::SUCCESS;
     }
 }
--- KataCola/utils/OrderCommand.php
+++ KataCola/utils/OrderCommand.php
@@ -39,13 +39,6 @@ class OrderCommand extends Command
             'yaml'
         );
 
-        /* @var CashFlow[] $cashFlow */
-        $cashFlow = $this->serializer->deserialize(
-            file_get_contents(__DIR__.'/cashFlow.yaml'),
-            CashFlow::class . '[]',
-            'yaml'
-        );
-
         /* @var float $actualCredit */
         $actualCredit = $this->serializer->deserialize(
             file_get_contents(__DIR__.'/credit.yaml'),
@@ -53,73 +46,14 @@ class OrderCommand extends Command
             'yaml'
         )->credit;
 
-
-        $productName = 'Please select a valid product';
-        foreach ($products as $product) {
-            if($product->keyName === $selectedProductKeyName){
-                $productName = $product->name;
-                break;
-            }
-        }
-
-        if($product->quantity  <= 0) {
-            $output->writeln('STOCK OUT OF: ' . $productName . ' please choose another product.');
-            return Command::SUCCESS;
-        }
-
-
-        $priceProduct = $product->price;
-        $moneyBack = 0;
-        if ($actualCredit < $priceProduct)
-        {
-            $output->writeln('trop pauvre');
-            return Command::SUCCESS;
-        }
-        else {
-            $moneyBack = $actualCredit - $priceProduct;
-        }
-        if ($moneyBack > 0) {
-
-            $toto = array_reverse($cashFlow);
-            $moneyBackCent = $moneyBack*100;
-            $moneyBack ='';
-            foreach ($toto as $singleCoin) {
-                if ($singleCoin->quantity > 0) {
-                    if (($moneyBackCent / $singleCoin->coin) > 1) {
-                        $nbCoin = floor($moneyBackCent / $singleCoin->coin);
-
-                        $moneyBack .= $nbCoin .' piece(s) de ' . $singleCoin->name.'\n';
-                        $moneyBackCent = $moneyBackCent % $singleCoin->coin;
-                    }
-
-                }
-
-            }
-
-            $returnCoin = ' Return -> '.$moneyBack;
-        }
-        else {
-            $returnCoin = '';
-        }
-
-
-        $product->quantity--;
-
-
-      $orderedProduct =  [$product->name => (array)$product];
-
-        $yaml = array_replace(
+        /*$yaml = array_replace(
             Yaml::parse(file_get_contents(__DIR__.'/products.yaml')),
             $orderedProduct
         );
-
         file_put_contents(__DIR__.'/products.yaml', Yaml::dump($yaml));
 
-
-       $output->writeln('PRODUCT-RETURN: ' . $productName . $returnCoin);
-
         $yaml = Yaml::dump(['credit' => 0.0]);
-        file_put_contents(__DIR__.'/credit.yaml', $yaml);
+        file_put_contents(__DIR__.'/credit.yaml', $yaml);*/
 
         return Command::SUCCESS;
     }
